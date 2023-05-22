3342330158d76c38edb1df521b3a7e6337959b5f
Elida Arnaut <elida.arnaut@gmail.com>
2023-05-22 23:01:40 +0200

    Created a branch for Planned Payment Input Page and added it to components

---
+++ frontend/components/PlannedPaymentInput.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { Dimensions } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView } from 'react-native';
+
+
+export default function PlannedPaymentInput() {
+
+    return (
+        <View></View>
+
+    );
+};
